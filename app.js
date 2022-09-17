9ba104032fc281087e22556eadf58884cbb6bb6e
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-09-17 15:46:50 -0700

    add images and preliminary structure

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-/* Imports */
+import * as THREE from './build/three.module.js';
+// import './styles/global.css';
 
-/* Get DOM Elements */
+const scene = new THREE.Scene();
+// console.log(THREE);
 
-/* State */
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-/* Events */
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.getElementById('bg'),
+});
 
-/* Display Functions */
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.getWorldPosition.setZ(30);
+renderer.render(scene, camera);
 
-// (don't forget to call any display functions you want to run on page load!)
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// // const sphereGeometry = new THREE.SphereGeometry(1.5, 32, 32);
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate();
