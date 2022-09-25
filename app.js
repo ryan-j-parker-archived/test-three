ab339381ebb874902d16320eacd562aa68ef8f9c
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-09-25 15:38:36 -0700

    add textures

--- app.js
+++ app.js
@@ -12,15 +12,8 @@ import { OrbitControls } from './jsm/controls/OrbitControls.js';
 // import * as dat from 'https://unpkg.com/dat.gui@0.7.7/build/dat.gui.module.js';
 
 
-// console.log(dat);
-
-
 //                                     GUI
-// const gui = new GUI({
-//     closed: true,
-//     width: 300,
-// });
-// gui.open();
+
 
 
 
@@ -37,8 +30,9 @@ import { OrbitControls } from './jsm/controls/OrbitControls.js';
 
 
 const scene = new THREE.Scene();
+// const glowScene = new THREE.Scene();
+// glowScene.add(new THREE.AmbientLight(0xffffff));
 
-// scene.add(gui);
 const camera = new THREE.PerspectiveCamera(
     55,
     window.innerWidth / window.innerHeight,
@@ -58,6 +52,14 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+
+const gui = new GUI({
+    width: 300,
+});
+scene.add(gui);
+
+// gui.add(sun.position, 'y').min(-10).max(10).step(0.01);
+// gui.open();
 
 const loadingManager = new THREE.LoadingManager();
 loadingManager.onStart = () => {
@@ -99,7 +101,29 @@ const uranusTexture = textureLoader.load('./assets/planets/uranus.jpg');
 const venusSurfaceTexture = textureLoader.load('./assets/planets/venus-surface.jpg');
 const venusAtmoTexture = textureLoader.load('./assets/planets/venus-atmosphere.jpg');
 const jupiterTexture = textureLoader.load('./assets/planets/jupiter.jpg');
+const earthNormal = textureLoader.load('./assets/planets/earth-normal.png');
+const earthSpecular = textureLoader.load('./assets/planets/earth-specular.png');
+const earthDay = textureLoader.load('./assets/planets/earth-day.jpg');
+const earthNight = textureLoader.load('./assets/planets/earth-night.jpg');
+const rockyHeight = textureLoader.load('./assets/rocky-height-map.png');
+const heightMap = textureLoader.load('./assets/height-map-1.png');
 
+// Water textures
+const waterNormal = textureLoader.load('./assets/water-textures/water-normal.jpg');
+const waterColor = textureLoader.load('./assets/water-textures/water-color.jpg');
+const waterDisp = textureLoader.load('./assets/water-textures/water-disp.png');
+const waterOcc = textureLoader.load('./assets/water-textures/water-occ.jpg');
+const waterSpec = textureLoader.load('./assets/water-textures/water-spec.jpg');
+
+// Alt. Earth textures
+const earthMap = textureLoader.load('./assets/planets/earthmap1k.jpg');
+const earthBump = textureLoader.load('./assets/planets/earthbump1k.jpg');
+const earthLights = textureLoader.load('./assets/planets/earthlights1k.png');
+const earthSpec = textureLoader.load('./assets/planets/earthspec1k.jpg');
+
+// Alt. Earth clouds textures
+const cloudMap = textureLoader.load('./assets/planets/earthcloudmap.jpg');
+const cloudMapTrans = textureLoader.load('./assets/planets/earthcloudmaptrans.jpg');
 
 const shinyTexture = textureLoader.load('./assets/shiny.png');
 
@@ -107,18 +131,64 @@ const marble = new THREE.Mesh(
     new THREE.SphereGeometry(2, 100, 100),
     new THREE.MeshMatcapMaterial({
         matcap: shinyTexture,
+        metalness: 0.45,
+        roughness: 0.45,
     }),
 );
 marble.position.set(0, 6, -24);
 scene.add(marble);
 
-scene.background = textureLoader.load('../assets/stars.jpg');
+scene.background = textureLoader.load('../assets/few-stars.jpg');
 
 
 
+// IN PROGRESS
+
+// const groundHeightTexture = textureLoader().load('./assets/height-map-1.png');
+// groundHeightTexture.wrapS = groundHeightTexture.wrapT = THREE.RepeatWrapping;
+// groundHeightTexture.repeat.set(0.5, -0.5);
+// groundHeightTexture.displacementScale = 0.4;
 
 
+// // const groundGeo =
+// const groundMesh = new THREE.Mesh(
+//     new THREE.PlaneGeometry(30, 30, 60, 60),
+//     new THREE.MeshStandardMaterial({
+//         map: venusSurfaceTexture,
+//         side: THREE.DoubleSide,
+//         normalMap: groundHeightTexture,
+//         // displacementMap: groundHeightTexture,
+//         // displacementScale: 0.8,
+//         // wrapS: THREE.RepeatWrapping,
+//         // wrapT: THREE.RepeatWrapping,
+//         // minFilter: THREE.LinearFilter,
+//         // needsUpdate: true,
 
+//     })
+// );
+// // const ground = new THREE.Mesh(groundGeo, groundMesh);
+// groundMesh.position.y = -10;
+// groundMesh.rotateX(Math.PI / 2);
+// scene.add(groundMesh);
+// // groundMesh.displacementMap.needsUpdate = true;
+
+// IN PROGRESS
+
+const waterGeo = new THREE.PlaneGeometry(30, 30, 60, 60);
+const waterMesh = new THREE.MeshStandardMaterial({
+    // color: 0xccddff,
+    map: waterColor,
+    normalMap: waterNormal,
+    displacementMap: waterDisp,
+    specularMap: waterSpec,
+    aoMap: waterOcc,
+    side: THREE.DoubleSide,
+});
+const water = new THREE.Mesh(waterGeo, waterMesh);
+water.position.y = -10;
+water.rotateX(Math.PI / 2);
+// water.rotateZ(180);
+scene.add(water);
 
 
 const bulbGeometry = new THREE.SphereGeometry(0, 16, 8);
@@ -134,18 +204,31 @@ bulbLight.position.set(0, 2, 0);
 bulbLight.castShadow = false;
 scene.add(bulbLight);
 
-const bulbGeometry2 = new THREE.SphereGeometry(0, 16, 8);
-const bulbLight2 = new THREE.PointLight(0xffee88, 4, 100, 2);
-const bulbMat2 = new THREE.MeshStandardMaterial({
-    emissive: 0xffffee,
-    emissiveIntensity: 1,
-    color: 0x000000,
-});
-bulbLight2.add(new THREE.Mesh(bulbGeometry2, bulbMat2));
-bulbLight2.position.set(0, -20, 0);
-// bulbLight2.castShadow = true;
-bulbLight.castShadow = false;
-scene.add(bulbLight2);
+// const bulbGeometry2 = new THREE.SphereGeometry(0, 16, 8);
+// const bulbLight2 = new THREE.PointLight(0xffee88, 4, 100, 2);
+// const bulbMat2 = new THREE.MeshStandardMaterial({
+//     emissive: 0xffffee,
+//     emissiveIntensity: 1,
+//     color: 0x000000,
+// });
+// bulbLight2.add(new THREE.Mesh(bulbGeometry2, bulbMat2));
+// bulbLight2.position.set(0, -20, 0);
+// // bulbLight2.castShadow = true;
+// bulbLight.castShadow = false;
+// scene.add(bulbLight2);
+
+// const bulbGeometry3 = new THREE.SphereGeometry(0, 16, 8);
+// const bulbLight3 = new THREE.PointLight(0xffee88, 4, 100, 3);
+// const bulbMat3 = new THREE.MeshStandardMaterial({
+//     emissive: 0xffffee,
+//     emissiveIntensity: 1,
+//     color: 0x000000,
+// });
+// bulbLight3.add(new THREE.Mesh(bulbGeometry3, bulbMat3));
+// bulbLight3.position.set(2, -10, -20);
+// // bulbLight3.castShadow = true;
+// bulbLight.castShadow = false;
+// scene.add(bulbLight3);
 
 // const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 // const material = new THREE.MeshBasicMaterial({ map: fireM, normalMap: fireM });
@@ -166,7 +249,7 @@ scene.add(bulbLight2);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
-renderer.render(scene, camera);
+renderer.render(scene, camera, gui);
 
 // camera.getWorldPosition.setZ(30);
 
@@ -254,24 +337,40 @@ scene.add(newMesh);
 
 const earth = new THREE.Mesh(
     new THREE.SphereGeometry(1, 40, 40),
-    new THREE.MeshPhongMaterial({
-        map: earthTexture,
-        clearcoat: 1,
-        reflectivity: 1,
+    new THREE.MeshStandardMaterial({
+        map: earthDay,
+        normalMap: earthMap,
+        displacementMap: earthBump,
+        displacementScale: 0.05,
+        // shadowMap: earthNight,
+        specularMap: earthSpec,
+        // clearcoat: 1,
+        // reflectivity: 1,
         // emissive: 0xffffff,
+        // emissiveIntensity: 0.12,
     }),
 );
+earth.rotateY(0.234);
+earth.rotateX(0.014);
+earth.rotateZ(0.03);
+earth.position.x = 0,
+    earth.position.y = 9,
+    earth.position.z = 19,
+    scene.add(earth);
+// earth.rotateY(23.5 * Math.PI / 180);
+
 
 const earthClouds = new THREE.Mesh(
-    new THREE.SphereGeometry(1.02, 40, 40),
-    new THREE.MeshPhongMaterial({
-        map: clouds,
-        transparent: true,
-        color: 0xffffff,
-        opacity: 0.35,
-        // emissive: 0xffffff,
-    }),
-);
+        new THREE.SphereGeometry(1.005, 40, 40),
+        new THREE.MeshPhongMaterial({
+            map: clouds,
+            // normalMap: cloudMapTrans,
+            transparent: true,
+            color: 0xffffff,
+            opacity: .55,
+            normalMap: earthDay,
+        }),
+    );
 
 const moon = new THREE.Mesh(
     new THREE.SphereGeometry(0.25, 20, 20),
@@ -310,7 +409,7 @@ const sun = new THREE.Mesh(
         // emissive: 0xffffff,
     }),
 );
-sun.add(earth, mercury);
+sun.add(mercury);
 scene.add(sun);
 
 
@@ -335,13 +434,14 @@ const tick = () => {
     // torus.rotation.y -= .04;
     // sphere.rotation.y = elapsedTime * Math.PI * 2;
 
-    earth.position.z = Math.sin(elapsedTime * 0.5) * 9;
-    earth.position.x = Math.cos(elapsedTime * 0.5) * 9;
+    // earth.position.z = Math.sin(elapsedTime * 0.5) * 9;
+    // earth.position.x = Math.cos(elapsedTime * 0.5) * 9;
     // earth.position.z = 5;
-    earth.rotation.y += 0.0365;
+    // earth.rotation.y += 0.0365;
     // moon.rotation.y += Math.PI * 0.0365;
 
     earthClouds.rotation.z += 0.0008;
+    earthClouds.rotation.y += 0.0008;
     // earthClouds.rotation.x += .0002;
     // earthClouds.rotation.y -= .0004;
 
@@ -362,33 +462,75 @@ const tick = () => {
     // sun.rotation.z -= .02;
 };
 
-earth.position.x = 0,
-earth.position.y = 0,
-earth.position.z = 10,
-
 tick();
 
 
-// const params = {
-//     color: 0xfff000,
-//     spin: () => {
-//         sun.rotation, {
-//             duration: 1,
-//             y: sun.rotation.y += Math.PI * 2
-//         };
-//     }
-// };
+const material = new THREE.MeshStandardMaterial();
+material.metalness = 0.7;
+material.roughness = 0.2;
+// material.wireframe = true;
+// material.color = 0xffaacc;
+
+const globe = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 64, 64),
+    material,
+);
+globe.position.set(1.5, 5, 15);
+
+scene.add(globe);
 
 
-// gui
-//     .add(params, 'spin');
 
-// gui
-//     .add(sun.rotation, 'y')
-//     .min(-30)
-//     .max(30)
-//     .step(0.1)
-//     .name('rotation');
+
+gui.add(material, 'metalness').min(0).max(1).step(0.0001);
+gui.add(material, 'roughness').min(0).max(1).step(0.0001);
+gui.add(material.wireframe, 'true');
+
+
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+
+const environmentMapTexture = cubeTextureLoader.load(
+    './assets/environmentMaps/0/px.jpg',
+    './assets/environmentMaps/0/nx.jpg',
+    './assets/environmentMaps/0/py.jpg',
+    './assets/environmentMaps/0/ny.jpg',
+    './assets/environmentMaps/0/pz.jpg',
+    './assets/environmentMaps/0/nz.jpg',
+);
+
+material.envMap = environmentMapTexture;
+
+const orbitRing = new THREE.Mesh(
+    new THREE.RingGeometry(2, 2, 50, 50, Math.PI * 2, Math.PI * 2),
+    new THREE.MeshStandardMaterial({
+        color: 0xffffff,
+
+    }),
+);
+orbitRing.rotation.z = Math.PI / 2;
+scene.add(orbitRing);
+
+
+const params = {
+    color: 0xfff000,
+    spin: () => {
+        sun.rotation, {
+            duration: 1,
+            y: sun.rotation.y += Math.PI * 2
+        };
+    }
+};
+
+
+gui
+    .add(params, 'spin');
+
+gui
+    .add(sun.rotation, 'y')
+    .min(-30)
+    .max(30)
+    .step(0.1)
+    .name('rotation');
 
 // gui
 //     .add(sun, 'visible');
@@ -400,45 +542,9 @@ tick();
 //     .add(sun.position.y, 'weird spin');
 
 // let sunSpeed = sun.position.x += 0;
-// gui
-//     .add(sun.position, 'y')
-//     .min(-30)
-//     .max(30)
-//     .step(0.1)
-//     .name('position');
-
-// gui
-//     .addColor(params, 'color')
-//     .onChange(() => {
-//         material.color.set(params.color);
-//     });
-
-// gui
-//     .add(sun.size, 'y')
-//     .min(-30)
-//     .max(30)
-//     .step(0.1)
-//     .name('size');
 
 
 
-// function animate() {
-//     requestAnimationFrame(animate);
-//     renderer.render(scene, camera);
-//     // torus.setRotationFromAxisAngle(new THREE.Vector3(0, 1, 0), 23.5);
-//     torus.rotation.y += .02;
-//     sphere.rotation.y = elapsedTime;
-
-// }
-
-// animate();
-
-
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-// // const sphereGeometry = new THREE.SphereGeometry(1.5, 32, 32);
-// const mesh = new THREE.Mesh(geometry, material);
-// scene.add(mesh);
 
 const cursor = {
     x: 0,
@@ -451,31 +557,12 @@ window.addEventListener('mousemove', (event) => {
     // console.log(event.clientX);
 });
 
-// const canvas = document.getElementById('bg');
-
-// window.addEventListener('resize', () => {
-//     canvas.width = window.innerWidth;
-//     canvas.height = window.innerHeight;
-
-//     camera.aspect = canvas.width / canvas.height;
-//     camera.updateProjectionMatrix();
-
-//     renderer.setSize(canvas.width, canvas.height);
-//     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-// });
-
-// window.addEventListener('resize', () => {
-//     camera.aspect = window.innerWidth / window.innerHeight;
-//     camera.updateProjectionMatrix();
-//     renderer.setSize(window.innerWidth, window.innerHeight);
-//     controls.handleResize();
-// });
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    // controls.handleResize();
+    controls.handleResize();
 }
 
 window.addEventListener('resize', onWindowResize, false);
